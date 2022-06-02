1a0ea83a0856945d996d1bad15dc43db880bb4de
hashmatullah nazari <hn0153@gmail.com>
2022-06-02 15:24:19 -0500

    navigation bar functional now

--- apps.js
+++ apps.js
@@ -1,6 +1,6 @@
 function myFunction() {
   var x = document.getElementById("myTopnav");
-  if(x.className === "topnav") {
+  if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
